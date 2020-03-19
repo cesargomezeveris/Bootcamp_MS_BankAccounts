cd642cb07e938ee243143a91a3ef7a278bef0701
vicse <vicse.ore@tecsup.edu.pe>
2020-03-18 23:31:05 -0500

    Project structure beginning

---
+++ src/main/java/com/vos/bootcamp/msbankaccounts/MsbankaccountsApplication.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.msbankaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbankaccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsbankaccountsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vos/bootcamp/msbankaccounts/MsbankaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.msbankaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbankaccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
