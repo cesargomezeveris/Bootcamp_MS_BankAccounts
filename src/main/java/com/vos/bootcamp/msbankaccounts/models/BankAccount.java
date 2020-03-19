d3a9d25bd529b84f20ebd897ed5355c295c648be
vicse <vicse.ore@tecsup.edu.pe>
2020-03-19 13:27:34 -0500

    Config Server, Models Mapped and correct checkStyle

---
+++ src/main/java/com/vos/bootcamp/msbankaccounts/models/BankAccount.java
@@ -0,0 +1,28 @@
+package com.vos.bootcamp.msbankaccounts.models;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "ms_bankAccounts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class BankAccount {
+
+  @Id
+  private String id;
+
+  @NotBlank(message = "'accountNumber' is required")
+  private String accountNumber;
+
+  @NotBlank(message = "'accountOwner' is required")
+  private String accountOwner;
+
+}
